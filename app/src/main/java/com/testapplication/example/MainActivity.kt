ad6bcc7edf82b696c8bc1de8cdbb36bee47338fb
muab <exp.mua@dsg.dk>
2019-09-08 23:24:04 +0500

    customize popup

--- app/src/main/java/com/testapplication/example/MainActivity.kt
+++ app/src/main/java/com/testapplication/example/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.testapplication.example
 
+import android.graphics.Typeface
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.TypedValue
 import android.view.View
 import com.testapplication.library.DialogBox
 
@@ -13,7 +15,21 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    fun showBox(view:View){
-        //DialogBox(this@MainActivity,"This is test message")
+    fun showBox(view: View) {
+        DialogBox(this@MainActivity)
+            .setTitleLabel("Error")
+            .setMessageLabel("This is test error message")
+            .setActionLabel("OK")
+            .setTitleColor(resources.getColor(R.color.vpi__background_holo_dark))
+            .setMessageColor(resources.getColor(R.color.vpi__background_holo_dark))
+            .setDividerColor(resources.getColor(R.color.color_standard_blue))
+            .setActionButtonColor(resources.getColor(R.color.vpi__background_holo_dark))
+            .setTitleTextSize(TypedValue.COMPLEX_UNIT_SP,18f)
+            .setMessageTextSize(TypedValue.COMPLEX_UNIT_SP,14f)
+            .setActionTextSize(TypedValue.COMPLEX_UNIT_SP,16f)
+            .setTitleTypeFace(Typeface.createFromAsset(applicationContext.assets, "font/museosans_300.ttf"))
+            .setMessageTypeFace(Typeface.createFromAsset(applicationContext.assets, "font/museosans_300.ttf"))
+            .setActionTypeFace(Typeface.createFromAsset(applicationContext.assets, "font/museosans_300.ttf"))
+            .showPopup()
     }
 }
--- library/src/main/java/com/testapplication/library/DialogBox.kt
+++ library/src/main/java/com/testapplication/library/DialogBox.kt
@@ -2,6 +2,7 @@ package com.testapplication.library
 
 import android.app.Dialog
 import android.content.Context
+import android.graphics.Typeface
 import android.view.View
 import android.widget.TextView
 
@@ -9,12 +10,15 @@ public class DialogBox : Dialog, View.OnClickListener {
 
     lateinit var textViewOk: TextView
     lateinit var textViewMessage: TextView
+    lateinit var textViewTitle: TextView
+    lateinit var dividerView: View
 
-    constructor(context: Context, message: String) : super(context, R.style.dialog_style_simple) {
+    constructor(context: Context) : super(context, R.style.dialog_style_simple) {
         setContentView(R.layout.test)
-        textViewMessage = findViewById(R.id.textview_error_message)
-        textViewMessage.setText(message)
+        textViewTitle = findViewById(R.id.textview_title)
+        textViewMessage = findViewById(R.id.textview_message)
         textViewOk = findViewById(R.id.textview_popup_ok)
+        dividerView = findViewById(R.id.divider_color)
         textViewOk.setOnClickListener(this)
     }
 
@@ -25,18 +29,90 @@ public class DialogBox : Dialog, View.OnClickListener {
         }
     }
 
-    fun setActionLabel(title: String):Dialog {
+    /*
+    Labels String assigned here
+     */
+    fun setTitleLabel(title: String): DialogBox {
+        textViewTitle.setText(title)
+        return this
+    }
+
+    fun setMessageLabel(title: String): DialogBox {
+        textViewMessage.setText(title)
+        return this
+    }
+
+    fun setActionLabel(title: String): DialogBox {
         textViewOk.setText(title)
         return this
     }
 
-    fun setActionButtonColor(color: Int):Dialog {
+
+    /*
+    Labels colors assigned here
+     */
+    fun setTitleColor(color: Int): DialogBox {
+        textViewTitle.setTextColor(color)
+        return this
+    }
+
+    fun setMessageColor(color: Int): DialogBox {
+        textViewMessage.setTextColor(color)
+        return this
+    }
+
+    fun setActionButtonColor(color: Int): DialogBox {
         textViewOk.setTextColor(color)
         return this
     }
 
-    fun setMessageColor(color: Int):Dialog {
-        textViewMessage.setTextColor(color)
+
+    /*
+    Fonts applied here
+     */
+
+    fun setTitleTypeFace(typeface: Typeface): DialogBox {
+        textViewTitle.typeface = typeface
+        return this
+    }
+
+    fun setMessageTypeFace(typeface: Typeface): DialogBox {
+        textViewMessage.typeface = typeface
+        return this
+    }
+
+    fun setActionTypeFace(typeface: Typeface): DialogBox {
+        textViewOk.typeface = typeface
+        return this
+    }
+
+    /*
+    Divider View
+     */
+
+    fun setDividerColor(color: Int): DialogBox {
+        dividerView.setBackgroundColor(color)
+        return this
+    }
+
+
+    /*
+
+    Text Sizes
+     */
+
+    fun setTitleTextSize(unit: Int,size:Float): DialogBox {
+        textViewTitle.setTextSize(unit,size)
+        return this
+    }
+
+    fun setMessageTextSize(unit: Int,size:Float): DialogBox {
+        textViewMessage.setTextSize(unit,size)
+        return this
+    }
+
+    fun setActionTextSize(unit: Int,size:Float): DialogBox {
+        textViewOk.setTextSize(unit,size)
         return this
     }
 
